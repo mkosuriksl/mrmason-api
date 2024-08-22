eb39886eba8b9ce02f2fe42cce742283090332ca
mkosuriksl <149906863+mkosuriksl@users.noreply.github.com>
2024-08-21 21:30:06 -0700

    Update UserService.java
    sms text updated

--- mrmason/src/main/java/com/application/mrmason/service/impl/UserService.java
+++ mrmason/src/main/java/com/application/mrmason/service/impl/UserService.java
@@ -72,7 +72,7 @@ public class UserService {
 		// Email sending
 		 sendEmail(user.getEmail());
 		// Mobile sms sending
-		String message = "Thanks for  registering with us. please verify your registered email and mobile.";
+		String message = "Thanks for registering with us. please verify your registered email and mobile before login. - mekanik.in";
 		smsService.sendSMSMessage(user.getMobile(), message);
 		User data = userDAO.save(user);
 

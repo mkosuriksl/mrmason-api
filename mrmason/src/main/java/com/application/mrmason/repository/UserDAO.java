69a581f31b6caa284600a3cb34270b2316ca3b33
Upendra Rai <upendrarai02@gmail.com>
2024-08-26 22:06:09 +0530

    minor issue fixed

--- mrmason/src/main/java/com/application/mrmason/repository/UserDAO.java
+++ mrmason/src/main/java/com/application/mrmason/repository/UserDAO.java
@@ -1,14 +1,14 @@
 package com.application.mrmason.repository;
 
 import java.util.List;
-//import java.util.Optional;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.application.mrmason.entity.User;
-//import com.application.mrmason.enums.RegSource;
+import com.application.mrmason.enums.RegSource;
 
 @Repository
 public interface UserDAO extends JpaRepository<User, String> {
@@ -18,7 +18,7 @@ public interface UserDAO extends JpaRepository<User, String> {
 	boolean existsByMobile(String mobile);
 
 	User findByEmail(String email);
-	
+
 	Optional<User> findByEmailAndRegSource(String email, RegSource regSource);
 
 	User findByMobile(String mobile);

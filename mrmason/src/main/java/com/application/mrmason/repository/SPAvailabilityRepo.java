c788c9d2e5bb6d6711e449da96641823c087d56e
Upendra Rai <upendrarai02@gmail.com>
2024-09-10 23:04:49 +0530

    minor issues fixed.

--- mrmason/src/main/java/com/application/mrmason/repository/SPAvailabilityRepo.java
+++ mrmason/src/main/java/com/application/mrmason/repository/SPAvailabilityRepo.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.application.mrmason.entity.SPAvailability;
@@ -12,6 +11,7 @@ import com.application.mrmason.entity.SPAvailability;
 @Repository
 public interface SPAvailabilityRepo extends JpaRepository<SPAvailability, Integer> {
 
+	@Query("SELECT s FROM SPAvailability s WHERE s.bodSeqNo = :bodSeqNo AND s.availability = 'yes'")
 	List<SPAvailability> findByBodSeqNo(String bodSeqNo);
 
 }

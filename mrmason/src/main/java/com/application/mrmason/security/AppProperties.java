19d4cec7aa1c215c8f191de93f398832724d4242
Upendra Rai <upendrarai02@gmail.com>
2024-10-06 23:20:29 +0530

    email creds taking from db and issues fixed .

--- mrmason/src/main/java/com/application/mrmason/security/AppProperties.java
+++ mrmason/src/main/java/com/application/mrmason/security/AppProperties.java
@@ -9,27 +9,6 @@ import lombok.Getter;
 @Configuration
 public class AppProperties {
 	
-//    @Value("${spring.mail.host}")
-//    private String mailHost;
-
-//    @Value("${spring.mail.port}")
-//    private int mailPort;
-//
-//    @Value("${spring.mail.username}")
-//    private String mailUsername;
-//
-//    @Value("${spring.mail.password}")
-//    private String mailPassword;
-//
-//    @Value("${spring.mail.properties.mail.smtp.auth}")
-//    private boolean mailSmtpAuth;
-//
-//    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
-//    private boolean mailSmtpStartTlsEnable;
-//
-//    @Value("${spring.web.resources.static-locations}")
-//    private String staticLocations;
-
     @Value("${mrmason.textLocal.api-key}")
     private String apiKey;
 

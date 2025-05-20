d818c20c10ffe0768385a43ce6dbd46c83dc6cd7
Renuka  Maradagi <86560767+Renukadeveloper96@users.noreply.github.com>
2025-05-20 13:40:33 +0530

    SP WORK ASSIGNMENT

--- mrmason/src/main/java/com/application/mrmason/service/SPWorkAssignmentService.java
+++ mrmason/src/main/java/com/application/mrmason/service/SPWorkAssignmentService.java
@@ -1,5 +1,7 @@
 package com.application.mrmason.service;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -7,8 +9,8 @@ import com.application.mrmason.entity.SPWorkAssignment;
 import com.application.mrmason.enums.RegSource;
 
 public interface SPWorkAssignmentService {
-    SPWorkAssignment createAssignment(SPWorkAssignment assignment,RegSource regSource);
+	List<SPWorkAssignment> createAssignment(SPWorkAssignment assignment,RegSource regSource);
 //    public List<SPWorkAssignment> getWorkers(String recId,String servicePersonId, String workerId, String updatedBy);
-    public Page<SPWorkAssignment> getWorkers(String recId, String servicePersonId, String workerId, String updatedBy, Pageable pageable);
+    public Page<SPWorkAssignment> getWorkers(String recId, String servicePersonId, String workerId, String updatedBy,String location,String available,String fromDateOfWork,String toDateOfWork,String spId, Pageable pageable);
     SPWorkAssignment updateWorkAssignment(SPWorkAssignment updatedAssignment,RegSource regSource);
 }

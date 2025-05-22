ea1d7b4bc35aa61f9459a2ed4233d0b20ad2ee96
Renuka  Maradagi <86560767+Renukadeveloper96@users.noreply.github.com>
2025-05-22 16:19:00 +0530

    AdminPaintTasksManagemnt api

---
+++ mrmason/src/main/java/com/application/mrmason/repository/AdminPaintTasksManagemntRepository.java
@@ -0,0 +1,9 @@
+package com.application.mrmason.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.application.mrmason.entity.AdminPaintTasksManagemnt;
+
+public interface AdminPaintTasksManagemntRepository extends JpaRepository<AdminPaintTasksManagemnt, String>{
+
+}

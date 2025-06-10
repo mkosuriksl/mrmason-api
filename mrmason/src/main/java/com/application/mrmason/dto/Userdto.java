378de5d6981ab8d076b66a5fd247d2ca9994355a
Renuka  Maradagi <renukamaradagi@gmail.com>
2025-06-10 12:12:13 +0530

    serviceType in loginResponse

--- mrmason/src/main/java/com/application/mrmason/dto/Userdto.java
+++ mrmason/src/main/java/com/application/mrmason/dto/Userdto.java
@@ -1,5 +1,7 @@
 package com.application.mrmason.dto;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +9,6 @@ import lombok.Setter;
 @Setter
 public class Userdto {
 
-	
 	public String bodSeqNo;
 	public String name;
 	public String businessName;
@@ -19,12 +20,13 @@ public class Userdto {
 	public String state;
 	public String location;
 	public String availableLocation;
-    public String updatedDate;
+	public String updatedDate;
 	public String registeredDate;
-    public String verified;
-    public String serviceCategory;
-    private String userType;
+	public String verified;
+	public String serviceCategory;
+	private String userType;
 	private String status;
 	private String regSource;
 	private String photo;
+	private List<String> serviceType;
 }

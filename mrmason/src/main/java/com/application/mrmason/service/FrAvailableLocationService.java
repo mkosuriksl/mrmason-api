f791eea5f334a4f7fa57d609b69693a3a20c67fb
Renuka <renukamaradagi@gmail.com>
2025-11-14 16:48:46 +0530

    fr location

--- mrmason/src/main/java/com/application/mrmason/service/FrAvailableLocationService.java
+++ mrmason/src/main/java/com/application/mrmason/service/FrAvailableLocationService.java
@@ -1,9 +1,14 @@
 package com.application.mrmason.service;
 
 import com.application.mrmason.dto.GenericResponse;
+import com.application.mrmason.dto.ResponseAvailableDto;
 import com.application.mrmason.entity.FrAvaiableLocation;
 
 public interface FrAvailableLocationService {
 
 	public GenericResponse<FrAvaiableLocation> addAvailableLocation(FrAvaiableLocation frAvaiableLocation);
+	public ResponseAvailableDto getAvailableLocations(
+	        String frUserId, String city, String countrycode,
+	        int page, int size);
+    public GenericResponse<FrAvaiableLocation> updateLocation(FrAvaiableLocation dto);
 }

d52238d6810e44d1ee5c8b32d75aaf8f59361b2e
Renuka <renukamaradagi@gmail.com>
2025-11-25 08:52:57 +0530

    get distinct material sub category by admin material master

--- mrmason/src/main/java/com/application/mrmason/repository/AdminMaterialMasterRepository.java
+++ mrmason/src/main/java/com/application/mrmason/repository/AdminMaterialMasterRepository.java
@@ -14,26 +14,11 @@ public interface AdminMaterialMasterRepository extends JpaRepository<AdminMateri
 	Optional<AdminMaterialMaster> findBySkuId(String skuId);
 
 	@Query("SELECT DISTINCT amm.brand FROM AdminMaterialMaster amm WHERE amm.materialCategory = :materialCategory And amm.materialSubCategory = :materialSubCategory")
-	List<String> findDistinctBrandByMaterialCategory(String materialCategory,String materialSubCategory);
-
-//	@Query("SELECT DISTINCT s.materialCategory FROM AdminMaterialMaster s WHERE s.materialCategory IS NOT NULL")
-//	List<String> findDistinctMaterialCategory();
-	
-	 @Query("SELECT s.materialCategory, s.materialSubCategory " +
-	           "FROM AdminMaterialMaster s " +
-	           "WHERE s.materialCategory IS NOT NULL AND s.materialSubCategory IS NOT NULL")
-	    List<Object[]> findCategoryAndSubCategory();
-
-//	@Query("SELECT m FROM AdminMaterialMaster m "
-//			+ "WHERE (:materialCategory IS NULL OR m.materialCategory = :materialCategory) "
-//			+ "AND (:materialSubCategory IS NULL OR m.materialSubCategory = :materialSubCategory) "
-//			+ "AND (:brand IS NULL OR m.brand = :brand)")
-//	List<AdminMaterialMaster> searchMaterials(@Param("materialCategory") String materialCategory,
-//			@Param("materialSubCategory") String materialSubCategory, @Param("brand") String brand);
+	List<String> findDistinctBrandByMaterialCategory(String materialCategory, String materialSubCategory);
 
 	List<AdminMaterialMaster> findByUpdatedBy(@Param("updatedBy") String updatedBy);
 
 	@Query("SELECT a FROM AdminMaterialMaster a WHERE a.skuId = :skuId")
-    List<AdminMaterialMaster> findBySkuIds(@Param("skuId") String skuId);
+	List<AdminMaterialMaster> findBySkuIds(@Param("skuId") String skuId);
 
 }
